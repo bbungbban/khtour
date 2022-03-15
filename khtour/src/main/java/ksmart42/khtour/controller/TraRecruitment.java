242f70f50faa8d4e47135e3213f1d91fec7f5c24
minseokKim0501 <klm7637@naver.com>
2022-03-15 20:43:16 +0900

    [team3] 20:43

--- khtour/src/main/java/ksmart42/khtour/controller/TraRecruitment.java
+++ khtour/src/main/java/ksmart42/khtour/controller/TraRecruitment.java
@@ -1,8 +1,10 @@
 package ksmart42.khtour.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class TraRecruitment {
 
 	/*
--- khtour/src/main/java/ksmart42/khtour/controller/TraShareController.java
+++ khtour/src/main/java/ksmart42/khtour/controller/TraShareController.java
@@ -1,5 +1,31 @@
 package ksmart42.khtour.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TraShareController {
+	/*
+	 * 여행기록 공유 게시판 등록
+	 */
+	@GetMapping("/traShare/traShareInsert")
+	public String addRecruitment(Model model) {
+		
+		model.addAttribute("title", "여행 기록 공유 게시판 등록 페이지");
+		
+		return "/traShare/traShareInsert";
+	}
+	
+	/*
+	 * 여행기록 공유 게시판 리스트
+	 */
+	@GetMapping("/traShare/traShareList")
+	public String getRecruitmentList(Model model) {
+		
+		model.addAttribute("title", "여행 기록 공유 게시판 리스트");
+		
+		return "/traShare/traShareList";
+	}
 
 }
