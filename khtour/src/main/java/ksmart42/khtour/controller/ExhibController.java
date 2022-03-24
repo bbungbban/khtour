6ae6b809d244df1f1f2813df46b0ff6414b1eb6e
김문영 <zxcv199698@naver.com>
2022-03-24 19:28:12 +0900

    [team3] 김문영 2022.03.24 19:27
    1.header_메인링크

--- khtour/src/main/java/ksmart42/khtour/controller/ExhibController.java
+++ khtour/src/main/java/ksmart42/khtour/controller/ExhibController.java
@@ -2,26 +2,30 @@ package ksmart42.khtour.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ksmart42.khtour.service.ExhibService;
+
 import org.springframework.ui.Model;
 
 @Controller
-public class ExhibitionController {
+@RequestMapping("/exhibition")
+public class ExhibController {
 	
-	@GetMapping("/exhibition/exhibInsert")
+	@GetMapping("/exhibInsert")
 	public String getExhibInsert(Model model) {
 		model.addAttribute("title", "특별전 등록");
 		
 		return "/exhibition/exhibInsert";
 	}
 	
-	@GetMapping("/exhibition/exhibList")
+	@GetMapping("/exhibList")
 	public String getExhibList(Model model) {
 		model.addAttribute("title", "특별전 검색 및 조회");
 		
 		return "/exhibition/exhibList";
 	}
 
-	@GetMapping("/exhibition/exhibModify")
+	@GetMapping("/exhibModify")
 	public String getExhibModify(Model model) {
 		model.addAttribute("title", "특별전 수정");
 		
