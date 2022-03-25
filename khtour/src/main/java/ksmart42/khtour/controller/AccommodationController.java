46285f4be6e4f0ca2dfe41a3843dcde2de67563c
bbungbban <shark1007@naver.com>
2022-03-25 15:02:10 +0900

    [team] 안창현 2022.03.25 지금 내가 뭐하는지를 모르겠으나 커밋!

--- khtour/src/main/java/ksmart42/khtour/controller/AccommodationController.java
+++ khtour/src/main/java/ksmart42/khtour/controller/AccommodationController.java
@@ -3,15 +3,17 @@ package ksmart42.khtour.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
+@RequestMapping("/accommodation")
 public class AccommodationController {
 	
 	/*
 	 * 숙박업소 등록	
 	 * */
-	@GetMapping("/accommodation/accommodationInsert")
+	@GetMapping("/accommodationInsert")
 	public String addaccommodation(Model model) {
 		
 		model.addAttribute("title", "숙박업소등록");
@@ -22,7 +24,7 @@ public class AccommodationController {
 	/*
 	 * 숙박업소 검색
 	 * */
-	@GetMapping("/accommodation/accommodationList")
+	@GetMapping("/accommodationList")
 	public String getaccommodation(Model model) {
 		
 		model.addAttribute("title", "숙박업소검색");
@@ -33,7 +35,7 @@ public class AccommodationController {
 	/*
 	 * 숙박업소정보 수정
 	 * */
-	@GetMapping("/accommodation/accommodationModify")
+	@GetMapping("/accommodationModify")
 	public String modifyaccommodation(Model model) {
 		
 		model.addAttribute("title", "숙박업소정보 수정");
@@ -44,7 +46,7 @@ public class AccommodationController {
 	/*
 	 * 숙박업소 삭제
 	 * */
-	@GetMapping("/accommodation/accommodationDelete")
+	@GetMapping("/accommodationDelete")
 	public String deleteaccommodation(Model model) {
 		
 		model.addAttribute("title", "숙박업소 삭제");
@@ -55,7 +57,7 @@ public class AccommodationController {
 	/*
 	 *숙박업소 정보
 	 * */
-	@GetMapping("/accommodation/acoommodationInfo")
+	@GetMapping("/acoommodationInfo")
 	public String Infoaccommodation(Model model) {
 		
 		model.addAttribute("title", "숙박업소 정보");
