b5af66d665de0fdf576ff345a8a3a4d62da95bb3
bbungbban <shark1007@naver.com>
2022-03-29 20:19:31 +0900

    [team]안창현 2022.03.29 20:19 리스트화면 작업

--- khtour/src/main/java/ksmart42/khtour/mapper/AccommodationMapper.java
+++ khtour/src/main/java/ksmart42/khtour/mapper/AccommodationMapper.java
@@ -1,12 +1,18 @@
 package ksmart42.khtour.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import ksmart42.khtour.dto.Accommodation;
 
 @Mapper
 public interface AccommodationMapper {
-	//숙박시설등록
+	//숙박업소 등록
 	public int addaccommodation(Accommodation accommodation);
+	
+	//숙박업소 리스트
+	public List<Accommodation> getAccommodationList(Map<String, Object> paramMap);
 
 }
