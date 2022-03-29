7a4964f0672de28bb2f409bc754a2636b719eebb
김문영 <zxcv199698@naver.com>
2022-03-29 15:38:42 +0900

    [team3] 김문영 2022.03.29  15:38 1.김문영개인작업

--- khtour/src/main/java/ksmart42/khtour/dto/Museum.java
+++ khtour/src/main/java/ksmart42/khtour/dto/Museum.java
@@ -1,97 +1,97 @@
 package ksmart42.khtour.dto;
 
 public class Museum {
-	private String ch_mus_code;
-	private String large_chmus_cate_code;
-	private String large_category;
-	private String ch_mus_name;
-	private String ch_mus_addr;
-	private String ch_mus_detail;
-	private String ch_mus_site;
-	private String ch_mus_num;
-	private String ch_mus_disclose;
-	private int ch_mus_start;
-	private int ch_mus_end;
-	private String ch_mus_close;
-	public String getCh_mus_code() {
-		return ch_mus_code;
+	private String chMusCode;
+	private String largeChmusCateCode;
+	private String largeCategory;
+	private String chMusName;
+	private String chMusAddr;
+	private String chMusDetail;
+	private String chMusSite;
+	private String chMusNum;
+	private String chMusDisclose;
+	private int chMusStart;
+	private int chMusEnd;
+	private String chMusClose;
+	public String getChMusCode() {
+		return chMusCode;
 	}
-	public void setCh_mus_code(String ch_mus_code) {
-		this.ch_mus_code = ch_mus_code;
+	public void setChMusCode(String chMusCode) {
+		this.chMusCode = chMusCode;
 	}
-	public String getLarge_chmus_cate_code() {
-		return large_chmus_cate_code;
+	public String getLargeChmusCateCode() {
+		return largeChmusCateCode;
 	}
-	public void setLarge_chmus_cate_code(String large_chmus_cate_code) {
-		this.large_chmus_cate_code = large_chmus_cate_code;
+	public void setLargeChmusCateCode(String largeChmusCateCode) {
+		this.largeChmusCateCode = largeChmusCateCode;
 	}
-	public String getLarge_category() {
-		return large_category;
+	public String getLargeCategory() {
+		return largeCategory;
 	}
-	public void setLarge_category(String large_category) {
-		this.large_category = large_category;
+	public void setLargeCategory(String largeCategory) {
+		this.largeCategory = largeCategory;
 	}
-	public String getCh_mus_name() {
-		return ch_mus_name;
+	public String getChMusName() {
+		return chMusName;
 	}
-	public void setCh_mus_name(String ch_mus_name) {
-		this.ch_mus_name = ch_mus_name;
+	public void setChMusName(String chMusName) {
+		this.chMusName = chMusName;
 	}
-	public String getCh_mus_addr() {
-		return ch_mus_addr;
+	public String getChMusAddr() {
+		return chMusAddr;
 	}
-	public void setCh_mus_addr(String ch_mus_addr) {
-		this.ch_mus_addr = ch_mus_addr;
+	public void setChMusAddr(String chMusAddr) {
+		this.chMusAddr = chMusAddr;
 	}
-	public String getCh_mus_detail() {
-		return ch_mus_detail;
+	public String getChMusDetail() {
+		return chMusDetail;
 	}
-	public void setCh_mus_detail(String ch_mus_detail) {
-		this.ch_mus_detail = ch_mus_detail;
+	public void setChMusDetail(String chMusDetail) {
+		this.chMusDetail = chMusDetail;
 	}
-	public String getCh_mus_site() {
-		return ch_mus_site;
+	public String getChMusSite() {
+		return chMusSite;
 	}
-	public void setCh_mus_site(String ch_mus_site) {
-		this.ch_mus_site = ch_mus_site;
+	public void setChMusSite(String chMusSite) {
+		this.chMusSite = chMusSite;
 	}
-	public String getCh_mus_num() {
-		return ch_mus_num;
+	public String getChMusNum() {
+		return chMusNum;
 	}
-	public void setCh_mus_num(String ch_mus_num) {
-		this.ch_mus_num = ch_mus_num;
+	public void setChMusNum(String chMusNum) {
+		this.chMusNum = chMusNum;
 	}
-	public String getCh_mus_disclose() {
-		return ch_mus_disclose;
+	public String getChMusDisclose() {
+		return chMusDisclose;
 	}
-	public void setCh_mus_disclose(String ch_mus_disclose) {
-		this.ch_mus_disclose = ch_mus_disclose;
+	public void setChMusDisclose(String chMusDisclose) {
+		this.chMusDisclose = chMusDisclose;
 	}
-	public int getCh_mus_start() {
-		return ch_mus_start;
+	public int getChMusStart() {
+		return chMusStart;
 	}
-	public void setCh_mus_start(int ch_mus_start) {
-		this.ch_mus_start = ch_mus_start;
+	public void setChMusStart(int chMusStart) {
+		this.chMusStart = chMusStart;
 	}
-	public int getCh_mus_end() {
-		return ch_mus_end;
+	public int getChMusEnd() {
+		return chMusEnd;
 	}
-	public void setCh_mus_end(int ch_mus_end) {
-		this.ch_mus_end = ch_mus_end;
+	public void setChMusEnd(int chMusEnd) {
+		this.chMusEnd = chMusEnd;
 	}
-	public String getCh_mus_close() {
-		return ch_mus_close;
+	public String getChMusClose() {
+		return chMusClose;
 	}
-	public void setCh_mus_close(String ch_mus_close) {
-		this.ch_mus_close = ch_mus_close;
+	public void setChMusClose(String chMusClose) {
+		this.chMusClose = chMusClose;
 	}
 	@Override
 	public String toString() {
-		return "Museum [ch_mus_code=" + ch_mus_code + ", large_chmus_cate_code=" + large_chmus_cate_code
-				+ ", large_category=" + large_category + ", ch_mus_name=" + ch_mus_name + ", ch_mus_addr=" + ch_mus_addr
-				+ ", ch_mus_detail=" + ch_mus_detail + ", ch_mus_site=" + ch_mus_site + ", ch_mus_num=" + ch_mus_num
-				+ ", ch_mus_disclose=" + ch_mus_disclose + ", ch_mus_start=" + ch_mus_start + ", ch_mus_end="
-				+ ch_mus_end + ", ch_mus_close=" + ch_mus_close + "]";
+		return "Museum [chMusCode=" + chMusCode + ", largeChmusCateCode=" + largeChmusCateCode + ", largeCategory="
+				+ largeCategory + ", chMusName=" + chMusName + ", chMusAddr=" + chMusAddr + ", chMusDetail="
+				+ chMusDetail + ", chMusSite=" + chMusSite + ", chMusNum=" + chMusNum + ", chMusDisclose="
+				+ chMusDisclose + ", chMusStart=" + chMusStart + ", chMusEnd=" + chMusEnd + ", chMusClose=" + chMusClose
+				+ "]";
 	}
 	
 }
