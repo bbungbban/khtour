29ba9c8c740857af04ffec2ce0807e86c5b33cd7
superuser <superuser@192.168.219.105>
2022-03-30 17:43:06 +0900

    [team3] 이민영 2022.03.30 17:43 1. 개인파일 수정

--- khtour/src/main/java/ksmart42/khtour/service/MemberService.java
+++ khtour/src/main/java/ksmart42/khtour/service/MemberService.java
@@ -1,30 +1,93 @@
 package ksmart42.khtour.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ksmart42.khtour.dto.LoginHistory;
 import ksmart42.khtour.dto.Member;
+import ksmart42.khtour.dto.MemberLevel;
 import ksmart42.khtour.mapper.MemberMapper;
 
 @Service
 @Transactional
 public class MemberService {
-
-	private MemberMapper memberMapper;
+	
+	//DI 의존성 주입 생성자메소드 주입방식
+	private MemberMapper memberMapper; 
 	
 	@Autowired
 	public MemberService(MemberMapper memberMapper) {
 		this.memberMapper = memberMapper;
 	}
 	
-	public List<Member> getMemberList(){
+	/**
+	 * 로그인 이력조회
+	 * @return Map<String, Object>
+	 */
+	public Map<String, Object> getLoginHistory(){
 		
-		List<Member> memberList = memberMapper.getMemberList();
+		List<LoginHistory> loginList = memberMapper.getLoginHistory1();
+		List<Map<String, Object>> loginMapList = memberMapper.getLoginHistory2();
+
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("loginList", loginList);
+		resultMap.put("loginMapList", loginMapList);
+		
+		return resultMap;
+	}
+	
+	public int modifyMember(Member member) {
+		return memberMapper.modifyMember(member);
+	}
+	
+	public Member getMemberInfoById(String memberId) {
+		return MemberMapper.getMemberInfoById(memberId);
+	}
+	
+	public List<Member> getMemberList(String searchKey, String searchValue){
+		
+		List<Member> memberList = memberMapper.getMemberList(searchKey, searchValue);
 		
 		return memberList;
+	}
+	
+	public int addMember(Member member) {
+		
+		int result = memberMapper.addMember(member);
+		
+		return result; 
+	}
+
+	public List<MemberLevel> getMemberLevelList() {
+		
+		List<MemberLevel> memberLevelList = memberMapper.getMemberLevelList();
+		
+		return memberLevelList;
+	}
+	//회원탈퇴
+	public void removeMember(Member member) {
+		//회원아이디
+		String memberId = member.getMemberId();
+		
+		//공통사항 삭제 프로세스
+		memberMapper.removeLoginHistory(memberId);
+		
+		memberMapper.removeMember(memberId);
 		
 	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
