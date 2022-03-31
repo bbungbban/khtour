095f1813e658642d9e301a2f9b791c2443dc6e0a
김문영 <zxcv199698@naver.com>
2022-03-31 16:00:14 +0900

    [team3] 김문영 2022.03.31  15:59 1.sidebar 김문영 개인부분 수정 2.김문영개인작업

--- khtour/src/main/java/ksmart42/khtour/controller/ExhibController.java
+++ khtour/src/main/java/ksmart42/khtour/controller/ExhibController.java
@@ -1,47 +1,105 @@
 package ksmart42.khtour.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import ksmart42.khtour.dto.Exhibition;
+import ksmart42.khtour.dto.Exhib;
 import ksmart42.khtour.service.ExhibService;
-import org.springframework.ui.Model;
 
 @Controller
+@RequestMapping("/exhib")
 public class ExhibController {
-	private ExhibService exhibService;
 
+	private ExhibService exhibService; 
+	
 	public ExhibController(ExhibService exhibService) {
 		this.exhibService = exhibService;
 	}
-
-	@GetMapping("/exhibition/exhibInsert")
-	public String addExhibition(Model model) {
-		model.addAttribute("title","특별전 등록");
+	
+	/*
+	 * 전시회 계획 조회 (관리자)(Get 정보 전달)
+	 */
+	@GetMapping("/exhibList")
+	public String getExhibList(Model model) {
+		Map<String, Object> paramMap = new HashMap<String , Object>();
 		
-		return "/exhibition/exhibInsert";
+		List<Exhib> exhibList = exhibService.getExhibList(paramMap);
+		
+		model.addAttribute("title", "전시회 계획 관리페이지");
+		model.addAttribute("exhibList", exhibList);
+		
+		return "exhib/exhibList";
 	}
 	
-	@PostMapping("/exhibition/exhibInsert")
-	public String addExhibition(Exhibition exhibition) {
+	/*
+	 * 전시회 계획 정보 수정 (관리자) (Post 정보 전달)
+	 */
+	@PostMapping("/modifyExhib")
+	public String modifyExhib(Exhib exhib) {
 		
-		exhibService.addExhibition(exhibition);
-		return "redirect:/exhibition/exhibList";
+		exhibService.modifyExhib(exhib);
+		System.out.println("정보 수정 포스트 전달" + exhibService.modifyExhib(exhib));
+		
+		return "redirect:/exhib/exhibList";
 	}
-
-	@GetMapping("/exhibition/exhibList")
-	public String getExhibList(Model model) {
-		model.addAttribute("title", "특별전 검색 및 조회");
-
-		return "/exhibition/exhibList";
+	
+	/*
+	 * 전시회 계획 정보 수정 (관리자) (Get 정보 전달)
+	 */
+	@GetMapping("/exhibModify")
+	public String modifyExhib(
+			@RequestParam(value="exhibCode", required = false) String exhibCode
+			,Model model) {
+		Exhib exhib = exhibService.getExhibByCode(exhibCode);
+		
+		model.addAttribute("title", "전시회 계획 수정 페이지");
+		model.addAttribute("exhib", exhib);
+		System.out.println("정보 수정 겟방식 전달" + exhib);
+		
+		return "exhib/exhibModify";
+	}	
+	
+	/*
+	 * 전시회 계획 정보 삭제(post 정보 전달)
+	 */
+	@GetMapping("/exhibRemove")
+	public String removeExhib(Exhib exhib) {
+		String exhibCode = exhib.getExhibCode();
+		
+		exhibService.removeExhib(exhibCode);
+		System.out.println("정보 삭제 포스트 전달" + exhibService.removeExhib(exhibCode));
+		
+		return "redirect:/exhib/exhibList";
+		
 	}
-
-	@GetMapping("/exhibition/exhibModify")
-	public String getExhibModify(Model model) {
-		model.addAttribute("title", "특별전 수정");
-
-		return "/exhibition/exhibModify";
+			
+	/*
+	 * 전시회 계획 등록(Post 정보 전달)
+	 */
+	@PostMapping("/exhibInsert")
+	public String addExhib(Exhib exhib) {
+		
+		exhibService.addExhib(exhib);
+		
+		return "redirect:/exhib/exhibList";
 	}
+	/*
+	 * 전시회 계획 등록(Get 정보 전달)
+	 */
+	@GetMapping("/exhibInsert")
+	public String addExhib(Model model) {
+		
+		model.addAttribute("title", "전시회 계획 등록");
+		
+		return "exhib/exhibInsert";
+	}
+	
 }
--- khtour/src/main/java/ksmart42/khtour/mapper/ExhibMapper.java
+++ khtour/src/main/java/ksmart42/khtour/mapper/ExhibMapper.java
@@ -1,12 +1,27 @@
 package ksmart42.khtour.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
-import ksmart42.khtour.dto.Exhibition;
-
+import ksmart42.khtour.dto.Exhib;
 
 @Mapper
 public interface ExhibMapper {
+
+	// 전시회 목록조회
+	public List<Exhib> getExhibList(Map<String, Object> paramMap);
+	
 	// 전시회 등록
-	public int addExhibition(Exhibition exhibition);
+	public int addExhib(Exhib exhib);
+
+	// 전시회 조회(전시회 코드)
+	public Exhib getExhibByCode(String exhibCode);
+	
+	// 전시회 수정(전시회코드)
+	public int modifyExhib(Exhib exhib);
+	
+	// 전시회 삭제
+	public int removeExhib(String exhibCode);
 }
