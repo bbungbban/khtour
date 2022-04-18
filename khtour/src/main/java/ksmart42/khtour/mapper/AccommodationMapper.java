ba44898aea887f57a0e3fd8c35b1310451b5ee54
bbungbban <shark1007@naver.com>
2022-04-18 21:54:48 +0900

    [team] 안창현 2022.04.18 21:54
    세부적인 부분 수정

--- khtour/src/main/java/ksmart42/khtour/mapper/AccommodationMapper.java
+++ khtour/src/main/java/ksmart42/khtour/mapper/AccommodationMapper.java
@@ -28,19 +28,26 @@ public interface AccommodationMapper {
 	//숙박업소 수정
 	public int modifyAccommodation(Accommodation accommodation);
 	
-	//숙박업소 리뷰 평점에 대한 평균 업데이트
-	public int updateGrade(String ldgCode);
-	
 	//숙박업소 평균별점 조회 
 	public String avgGrade(String ldgCode);
-	
 	//숙박업소 청결도평균 점수 조회
 	public String avgCleanliness(String ldgCode);
+	//숙박업소 친절도평균 점수 조회
+	public String avgKindness(String ldgCode);
+	//숙박업소 편의성평균 점수 조회
+	public String avgConvenience(String ldgCode);
+	//숙박업소 위치만족도평균 점수 조회
+	public String avgLocation(String ldgCode);
+	//숙박업소 가격만족도평균 점수 조회
+	public String avgPriceSta(String ldgCode);
 	
 	//숙박업소 리뷰 + 
 	public void addReviewCnt(String ldgCode);
 	
 	//숙박업소 리뷰 - 
 	public void subtractReviewCnt(String ldgCode);
+	
+	//숙박업소 중복체크
+	public boolean isNameCheck(String ldgName);
 	
 }
