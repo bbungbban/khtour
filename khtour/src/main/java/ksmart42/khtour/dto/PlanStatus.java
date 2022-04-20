19b513cec6572cfdc922364750b4bf73e46a17b8
김민석 <klm7637@naver.com>
2022-04-20 13:26:00 +0900

    test

--- khtour/src/main/java/ksmart42/khtour/dto/PlanStatus.java
+++ khtour/src/main/java/ksmart42/khtour/dto/PlanStatus.java
@@ -3,14 +3,12 @@ package ksmart42.khtour.dto;
 public class PlanStatus {
 	//plan_status_code,
 	//plan_status_name,
-	//reg_date,
-	//plan_status_cnt
+	//reg_date
 	
 	
 	private String planStatusCode;
 	private String planStatusName;
 	private String regDate;
-	private String planStatusCnt;
 	
 	
 	public String getPlanStatusCode() {
@@ -31,16 +29,10 @@ public class PlanStatus {
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
-	public String getPlanStatusCnt() {
-		return planStatusCnt;
-	}
-	public void setPlanStatusCnt(String planStatusCnt) {
-		this.planStatusCnt = planStatusCnt;
-	}
 	@Override
 	public String toString() {
 		return "PlanStatus [planStatusCode=" + planStatusCode + ", planStatusName=" + planStatusName + ", regDate="
-				+ regDate + ", planStatusCnt=" + planStatusCnt + "]";
+				+ regDate + ", planStatusCnt=" + "]";
 	}
 	
 }
